63b0018a43150729ccec9a6fc0bc16d3c25a17ed
wiphodfx <wifidifx@gmail.com>
2022-09-08 10:22:14 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase with proper grammer,
+ * but the outcome is a piece of art,
+ * followed by a new line.
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
