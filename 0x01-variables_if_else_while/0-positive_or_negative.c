c0fbe580e296a5c479588f1d6f55700dc8bb2613
wiphodfx <wifidifx@gmail.com>
2022-09-09 07:55:31 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /*Main function for execution of program*/
-int main(void) /*This is the main function of the program*/
+int main(void)/*This is the main function of the program that executes it*/
 {	/*Entry point of main function*/
 	int n;
 
@@ -16,7 +16,7 @@ int main(void) /*This is the main function of the program*/
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	}		
+	}
 	else
 	{
 		printf("%d is negative\n", n);
