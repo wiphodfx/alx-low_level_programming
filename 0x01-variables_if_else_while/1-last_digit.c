57f347c054d23ae44b4d57eb49d8a7637cfd2c03
wiphodfx <wifidifx@gmail.com>
2022-09-09 13:14:44 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,8 +4,8 @@
 /**
  * main assigns a random number
  * to the variable "n" each time it is executed
- * to print the last digit of the number stored in the
- * variable n.
+ * to print the last digit n of the number stored in the
+ * variable "n".
  * Return: Always 0 (success)
  */
 int main(void)
