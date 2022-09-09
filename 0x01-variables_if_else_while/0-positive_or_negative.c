7ab6c3ba7b8f8b9f1e330fc8a7fc07ebf49eb713
wiphodfx <wifidifx@gmail.com>
2022-09-09 07:33:48 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,27 +4,26 @@
 /* more headers goes there */
 
 /* This is the main function of the program*/
-int main(void)
+int main(void) /*This is the main function of the program */
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if(n > 0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if(n == 0)
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-
 	}		
 	else
 	{
 		printf("%d is negative\n", n);
 	}
 
-		return 0;
+		return (0);
 }
 
