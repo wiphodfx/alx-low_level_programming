e0995bcdf0fe56acfd1746cac677723837eac784
wiphodfx <wifidifx@gmail.com>
2022-09-09 20:51:09 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * A program that prints
+ * alphabets in lowercase
+ * using the putchar fucnction 
+ * and Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar("Value %d char %c\n" c, c);
+		c = c+1;
+	}
+	exit (EXIT_SUCCESS);
+}
