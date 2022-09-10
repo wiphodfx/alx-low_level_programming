9a1d86444cbec5189101027fe6f47b900d35d106
wiphodfx <wifidifx@gmail.com>
2022-09-09 23:32:36 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlibh>
+/**
+ * main - prints alphabets in lowercase in a reverse order
+ * followed by a newline
+ * Return Always0 (success)
+ */
+int main(void)
+{
+	char ck;
+
+	for (ck ='a'; ck > 'a'; ck--)
+	{
+		putchar(ck);
+	}
+	putchar('\n');
+	return (0);
+}
