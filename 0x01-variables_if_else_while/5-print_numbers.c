44512bd5930634b7880898130e0a0cca840bf5f6
wiphodfx <wifidifx@gmail.com>
2022-09-09 22:14:56 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * followed by a new line
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for ( i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
