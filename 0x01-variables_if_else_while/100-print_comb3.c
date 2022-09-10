5590d9cb03767a1d4d08d74ad502b281569e603b
wiphodfx <wifidifx@gmail.com>
2022-09-10 01:12:55 -0700

    Task adv

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all possible different combinations of two digits
+ * seperated by , and a space
+ * 01 and 10 are considered the same
+ * prints only the smallest combination of the two digits
+ * numbers printed in ascending order
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int k;
+	int y;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (k = 49; k < 59; k++)
+		{
+			if (k > i)
+			{
+				putchar(i);
+				putchar(k);
+				if (i != 57 || k != 58)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
