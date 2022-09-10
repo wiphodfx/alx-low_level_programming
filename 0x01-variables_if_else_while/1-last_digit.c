180b203bf45d6a64fd8d98174143d97d631252d4
wiphodfx <wifidifx@gmail.com>
2022-09-09 20:16:41 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,6 +21,9 @@ int main(void)
 	{
 		printf("Last digit of %d is -8 and is less than 6 and not 0\n", n);
 	}
-	printf("Last digit of %d is 0 and is 0\n", n);
+	else
+	{
+		printf("Last digit of %d is 0 and is 0\n", n);
+	}
 	return (0);
 }
