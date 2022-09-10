f5e4355329879de8bf703f35a453e1c2c31a1b49
wiphodfx <wifidifx@gmail.com>
2022-09-09 22:01:27 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints alphabets in lowercase and then in uppercase
+ * followed by a newline
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char cz = 'a';
+	char cx = 'A';
+	for(cz = 'a'; cz <= 'z'; cz++)
+	{
+		putchar(cz);
+
+		for (cx = 'A'; cx <= 'Z'; cx++)
+		{
+			putchar(cx);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
