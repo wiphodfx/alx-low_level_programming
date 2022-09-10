3e92bc2ee75ec28f220968a4779103b1a0c7bf75
wiphodfx <wifidifx@gmail.com>
2022-09-10 02:00:24 -0700

    Task adv 2

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all the posible combinations of three digits
+ * separated by a , and space
+ * followed by a newline
+ * the three digits must be different
+ * prints the smallest combination of the three digits
+ * nubers should be printed in ascending order
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 45; i <= 56; i++)
+	{
+		for (j = 46; j <= 57; j++)
+		{
+			for (k = 47; k <= 58; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i > k && k > j)
+				{
+					if (i != 55 && k != 56 && j != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					
+					}
+				}
+			}
+			putchar('\n');
+			return (0);
+}
