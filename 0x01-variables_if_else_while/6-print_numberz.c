88cc34d7d6d9fd1c0bb62e610ce2e1eb637718e5
wiphodfx <wifidifx@gmail.com>
2022-09-09 22:44:24 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  main- prints all single digits of base 10 starting from 0
+ *  followed by a newline
+ *  Return: always 0 (success)
+ */
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchr('\n');
+	return (0);
+}
