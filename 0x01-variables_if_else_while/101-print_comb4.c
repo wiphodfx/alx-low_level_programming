ea64cc2ba8dfd8b3c6bc863f6ce4bb68e117cf64
wiphodfx <wifidifx@gmail.com>
2022-09-10 02:51:04 -0700

    task adv2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,17 +13,17 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 48; i <= 54; i++)
+	for (i = 48; i <= 58; i++)
 	{
-		for (j = 49; j <= 55; j++)
+		for (j = 49; j <= 58; j++)
 		{
-			for (k = 50; k <= 56; k++)
+			for (k = 50; k <= 58; k++)
 			{
-				putchar(i);
-				putchar(j);
-				putchar(k);
 				if (k > j && j > i)
 				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
 					if (i != 55 || k != 56)
 					{
 						putchar(',');
