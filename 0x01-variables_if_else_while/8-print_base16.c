73742f2d5431f997d6cec6272379a385a6982a9c
wiphodfx <wifidifx@gmail.com>
2022-09-09 23:54:33 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ * followed by a newline
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	char ck;
+
+	for (i = 28; i <= 38; i++)
+	{
+		putchar(i);
+	}
+	for (ck = 'a'; ck < 'z'; ck++)
+	{
+		putchar(ck);
+	}
+
+	putchar('\n');
+	return (0);
+}
