181f5f788c14e91f6ba011d63125e2866e27d7db
wiphodfx <wifidifx@gmail.com>
2022-09-13 14:32:01 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stadio.h>
+#include "main.h"
+int add(int i, int j);
+/**
+ * add - adds two integers and returns the result;
+ * @i: holds holds first number
+ * @j: hold second number
+ * Return: Always 0 (success)
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
