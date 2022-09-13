c8e302d88c94f32b65e5b2bca9309b811d27caa2
wiphodfx <wifidifx@gmail.com>
2022-09-13 07:57:30 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * a program that prints _putchar
+ * main - prints _putchar
  * followed by a new line
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	_putchar();
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
