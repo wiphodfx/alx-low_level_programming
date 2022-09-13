dd1b1a2c311987b0514aceee591e3adfe78005dc
wiphodfx <wifidifx@gmail.com>
2022-09-13 12:18:34 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "main.h"
+void times_table(void);
+/**
+ * times_table - prints the 9 times table
+ * starting with 0
+ * Return: Alway 0 (success)
+ */
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+			if ((k / 10) == 0)
+			{
+				if (j == 0)
+				{
+					_putchar('0');
+				}
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+				if (j < 9)
+				{
+					_putchar(',');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
+
+
