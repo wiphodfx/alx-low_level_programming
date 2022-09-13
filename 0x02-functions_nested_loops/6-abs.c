61c52cfd3a83016837b3d73897f21ab3b0388e53
wiphodfx <wifidifx@gmail.com>
2022-09-13 11:05:21 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+int _abs(int);
+/**
+ * _abs - computes the absolute values of integers
+ * int - parameter to hold value
+ * Return: Always 0 (success)
+ */
+int _abs(int)
+{
+	if (int % 10 >= 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
