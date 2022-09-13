4dbcc4cc687df1369eac7878bba5f581147ac4da
wiphodfx <wifidifx@gmail.com>
2022-09-13 12:03:35 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 #include "main.h"
 void jack_bauer(void);
 /**
- * jac_bauer - prints every minute of the day of Jack Bauer
+ * jack_bauer - prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59
  * Return: Alway 0 (success)
  */
@@ -19,7 +19,7 @@ void jack_bauer(void)
 			_putchar(i % 10 + '0');
 			_putchar(':');
 			_putchar(k / 10 + '0');
-			_putchar(k % 10 +'0');
+			_putchar(k % 10 + '0');
 			_putchar('\n');
 		}
 	}
