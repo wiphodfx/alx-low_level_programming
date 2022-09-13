131df4cd92a22c97500b5bf0b070983a4cc60f90
wiphodfx <wifidifx@gmail.com>
2022-09-13 08:58:58 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+int _islower(int c);
+/**
+ * _islower - function that checkes for lower character
+ * returns 1 if c is lowercase;
+ * returns 0 otherwise
+ * Return: Always 0 (success)
+ */
+int _islower(int c)
+{
+	char ck = _islower();
+
+	if (ck == 'c')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
