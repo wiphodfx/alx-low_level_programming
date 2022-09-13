95884c6b6a2c1b8014cfe205a17789b522443014
wiphodfx <wifidifx@gmail.com>
2022-09-13 11:38:25 -0700

    Task 6 & 7

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
 int _abs(int i);
 /**
  * _abs - computes the absolute values of integers
- * @i - hold absolute value of int to print
+ * @i: hold absolute value of int to print
  * Return: Always 0 (success)
  */
 int _abs(int i)
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+int print_last_digit(int i);
+/**
+ * print_last_digit - print last digit of a number
+ * @i: stres the last digit to print
+ * Return: Always 0 (success)
+ */
+int print_last_digit(int i)
+{
+	int lastDigit;
+
+	if (i < 0)
+	{
+		lastDigit = (-1 * (n % 10));
+		_putchar(lastDigit + '0');
+		return (lastDigit);
+	}
+	else
+	{
+		lastDigit = (n % 10);
+		_putchar(lastDigit +'0');
+		return (lastDigit);
+	}
+}
