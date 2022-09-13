8695206015c558ce81319cea3d41c52b373c9c09
wiphodfx <wifidifx@gmail.com>
2022-09-13 08:39:17 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,7 @@ void print_alphabet_x10(void);
  * followed by a new line
  * Return: Always 0 (success)
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void) /* print_alphabet_x10 prototype function*/
 {
 	char ck;
 	int i = 0;
@@ -15,7 +15,7 @@ void print_alphabet_x10(void)
 	while (i < 10)
 	{
 		ck = 'a';
-		while (ck < 'z')
+		while (ck <= 'z')
 		{
 			_putchar(ck);
 			ck++;
