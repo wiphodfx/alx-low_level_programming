a5f625df914393af6353da3506ce14767178d4ee
wiphodfx <wifidifx@gmail.com>
2022-09-13 05:02:15 -0700

    Task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,13 @@
-void _putchar();
-void print_alphabet(void);
+#include <stdio.h>
+#include "main.h"
+void print_alphabet(void)
+{
+	char ck;
+
+	for (ck = 'a'; ck > 'z'; ck++)
+	{
+		putchar(ck);
+	}
+	putchar('\n');
+	return (0);
+}
