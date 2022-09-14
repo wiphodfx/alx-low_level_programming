1bf84fd09818067df800ed9f0bb7a7f12bb4541a
wiphodfx <wifidifx@gmail.com>
2022-09-14 06:58:06 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,8 +3,11 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * main- test the function positive_or_negative function
+ * main.c - test that the function positive_or_negative function
  * gives the correct output when given a case of 0
+ * main - runs the loop to genate the numbers
+ * positi_or_negative - function test the number stored in n
+ * @n: stores the number from the Rand() loop calculation
  * Return: Always 0 (success)
  */
 int main(void)
