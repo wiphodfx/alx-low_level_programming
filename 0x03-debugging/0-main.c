86ae638b793f27f864d89322db686a8ad42bb2a4
wiphodfx <wifidifx@gmail.com>
2022-09-14 07:26:23 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 /**
  * main - test the function positive_or_negative function
- * 0-main.c - file test that the function positive_or_negative gives the right output
+ * main.c - file test that the function positive_or_negative gives the right output
  * positive_or_negative() - function test the number stored in n
  * @n: holds the  number to be tested
  * Return: Always 0 (success)
@@ -14,7 +14,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n = ((rand() - RAND_MAX) / 2);
 	/* positive_or_negative function test the value of n*/
 	positive_or_negative(n);
 	return (0);
