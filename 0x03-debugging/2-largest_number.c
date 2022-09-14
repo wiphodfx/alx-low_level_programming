54a50c962a2877d83895bdc4a908260c307c6a3a
wiphodfx <wifidifx@gmail.com>
2022-09-14 13:23:00 -0700

    Task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,8 +2,11 @@
 #include "main.h"
 
 /**
- * main - prints the largest of 3 numbers
- * Return: 0
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
  */
 
 int largest_number(int a, int b, int c)
