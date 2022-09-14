49298e642a27637ae4ef759096ec16e102b8ef86
wiphodfx <wifidifx@gmail.com>
2022-09-14 06:43:12 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main- test the function positive_or_negative function
+ * gives the correct output when given a case of 0
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+
+	positive_or_negative(i);
+	return (0);
+}
