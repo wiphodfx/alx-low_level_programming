5f627532892048d6d3007943a12f91420d80bbd2
wiphodfx <wifidifx@gmail.com>
2022-09-14 03:51:32 -0700

    Task 0

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main- test the function positive_or_negative function
+ * gives the correct output when given a case of 0
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int r = 0;
+
+	positive_or_negative(r);
+	return (0);
+
+}
