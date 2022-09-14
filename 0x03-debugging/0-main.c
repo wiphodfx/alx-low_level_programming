fb8803c9360709dd7f8f6b137df1d13a684113a8
wiphodfx <wifidifx@gmail.com>
2022-09-14 11:56:00 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,15 @@
-#include <stdio.h>
-#include <time.h>
 #include "main.h"
-#include <stdlib.h>
+
 /**
- * main - test function that prints if integer is positive or negative
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
-	/* positive_or_negative function that test the value of i*/
 	positive_or_negative(i);
 
 	return (0);
