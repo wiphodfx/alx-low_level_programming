8378f3901009998cfb8857bd7a97fde46a280e8b
wiphodfx <wifidifx@gmail.com>
2022-09-15 09:49:22 -0700

    Task

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -9,14 +9,6 @@
  */
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-	return (0);
+	int result = if (c >= '0' && c <= '9');
+	return (result);
 }
-
