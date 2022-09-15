761ce8c99700baef0be88347800a21705c10cd50
wiphodfx <wifidifx@gmail.com>
2022-09-15 10:13:55 -0700

    Task

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,8 +2,8 @@
 #include "main.h"
 
 /**
- * _isdigit - function that checks for a digit(0 through 9)
- * @c: contains digit to be checked
+ * _isdigit - checks for a digit(0 through 9)
+ * @c: int digit to be checked
  * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,4 +4,14 @@
 #include "main.h"
 
 int _isupper(int c);
+int _isdigit(int c);
+int _mul(int 1, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_traingle(int size);
+
 #endif /* MAIN_H*/
