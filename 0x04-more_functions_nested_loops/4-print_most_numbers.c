9ff6e65bb411fb5f550a77f3d9d07e54bf643a0e
wiphodfx <wifidifx@gmail.com>
2022-09-15 14:29:29 -0700

    task

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,11 +4,11 @@ void print_most_numbers(void);
 
 /**
  * print_most_numbers - prints the numbers, from 0 to 9,
- * except 2 and 4 followed by a new line
+ * except 2 and 4, followed by a new line
  */
 void print_most_numbers(void)
 {
-	int i;
+	int i = 0;
 
 	while (i <= 9)
 	{
