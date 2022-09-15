7622e918e15f72d67c07767005b9ea698184f5b5
wiphodfx <wifidifx@gmail.com>
2022-09-15 15:12:53 -0700

    task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -18,8 +18,8 @@ void more_numbers(void)
 		{
 			if(j >= 9)
 			{
-				_putchar(j / 9 + '0');
-				_putchar(j % 9 + '0');
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
 			}
 			j++;
 		}
