04c452bc2cb4384b34ee603b4fa6b6aaa15b3cec
wiphodfx <wifidifx@gmail.com>
2022-09-15 14:43:29 -0700

    task

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+void more_numbers(void);
+
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14, followed by a new line
+ */
+void more_numbers(void)
+{
+	int i = 0;
+	int j;
+
+	while (i <= 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			_putchar(j + '0');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
