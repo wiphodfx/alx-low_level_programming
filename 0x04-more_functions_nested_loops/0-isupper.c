b3a559d63a5bc33f42ef70ed36719156c8b0733a
wiphodfx <wifidifx@gmail.com>
2022-09-15 08:11:09 -0700

    Task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 int _isupper(int c);
+
 /**
  * _isupper - function that checks for uppercase character
  * @c: contains value to be compared
- * returns: 1 if c is uppercase
- * returns: 0 otherwise
- * Return: Always 0 (success)
+ * Returns: 1 if c is uppercase
+ * Returns: 0 otherwise
+ * Return: 0
  */
-
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
