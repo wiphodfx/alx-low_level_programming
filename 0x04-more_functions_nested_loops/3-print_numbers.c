1cdc52f31996779f2b8f73223e4b6c1a12ff8ba2
wiphodfx <wifidifx@gmail.com>
2022-09-15 12:45:32 -0700

    task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,8 +3,7 @@
 void print_numbers(void);
 
 /**
- * print_numbers - prints the numbers from 0 to 9
- * followed by a new line
+ * print_numbers - prints the numbers from 0 to 9, followed by a new line
  * Return: 0
  */
 void print_numbers(void)
