0df8e493e2dd23661e6b612a00eb65701bffba7f
wiphodfx <wifidifx@gmail.com>
2022-09-15 15:02:33 -0700

    task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,13 @@ void more_numbers(void)
 	int i = 0;
 	int j;
 
-	while (i <= 10)
+	while (i <= 9)
 	{
 		j = 0;
 
 		while (j <= 14)
 		{
-			if (j >= 10)
+			if (j >= 9)
 			{
 				_putchar(j / 10 +'0');
 				_putchar(j % 10 + '0');
