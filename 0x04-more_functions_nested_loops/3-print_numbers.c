76474c80f9348b8f5ee51a664bd9625c75dcb96a
wiphodfx <wifidifx@gmail.com>
2022-09-15 12:13:37 -0700

    task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+void print_numbers(void);
+
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ * followed by a new line
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
