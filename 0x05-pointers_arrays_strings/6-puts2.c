62d4b834fc79fc451fde77e6b7a9ec0cc83bf8f9
wiphodfx <wifidifx@gmail.com>
2022-09-19 11:38:49 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string starting with the first character
+ * puts2 - prints every other character of a string
+ * starting with the first character
  * followed by a new line
  * @str: stores the string
  * Return: void
