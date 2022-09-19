0bdc4f98edded8ad26bd0d56f94c235e402c2b2c
wiphodfx <wifidifx@gmail.com>
2022-09-19 08:22:34 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: stores the string
+ * Return: the length of string
+ */
+int _strlen(char *s)
+{
+	return (sizeof(s));
+}
