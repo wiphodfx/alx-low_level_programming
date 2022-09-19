b3f39498bd38af5f490aac745c4b23e584ba0b46
wiphodfx <wifidifx@gmail.com>
2022-09-19 11:43:01 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: stores the string to be printed half
+ * Return: void
+ */
+void puts_half(char *str)
+{
+}
+
