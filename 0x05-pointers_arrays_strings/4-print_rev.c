aa37a1ddb68bcaf81d96bac8259cbbc93e611768
wiphodfx <wifidifx@gmail.com>
2022-09-19 11:03:57 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,12 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	int i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
 	for (i = i - 1 ; i >= 0; i--)
 	{
 		_putchar (s[i]);
