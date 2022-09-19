e8a35c1bdbddf99015a68f8ab7742b500ff2d987
wiphodfx <wifidifx@gmail.com>
2022-09-19 11:21:32 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: stores the string to be reversed
+ * Retun: void
+ */
+void rev_string(char *s)
+{
+	char *first_character, *last_character, c;
+	int i, timer;
+	int length = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		length++;
+	}
+
+	timer = length;
+
+	first_character = s;
+	last_character = s;
+
+	for (i = 0; i < timer - 1; i++)
+	{
+		last_character++;
+	}
+
+	for (i = 0; i < timer / 2; i++)
+	{
+		c = *last_character;
+		*last_character = *first_character;
+		*first_character = c;
+
+		first_character++;
+		last_character++;
+	}
+}
