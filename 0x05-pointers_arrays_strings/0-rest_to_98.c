e62c1a97b87efc50c0adea5f9ffc80a3d009eb29
wiphodfx <wifidifx@gmail.com>
2022-09-19 06:14:34 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-rest_to_98.c
+++ 0x05-pointers_arrays_strings/0-rest_to_98.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- * void reset_to_98 - take a pointer to an int as parameter 
+ * reset_to_98 - take a pointer to an int as parameter
  * and updates the value it points to 98.
- *
+ * @n: store the value it points to
  * Return nothing
  */
 void reset_to_98(int *n)
