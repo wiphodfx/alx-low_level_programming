8b92abaf32f34d517022ec62b49ff18105d5fbf8
wiphodfx <wifidifx@gmail.com>
2022-09-19 09:42:35 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _puts - prints a string, followed by a new line
+ * to stdout.
+ * @str: holds the string
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	_puts('p');
+	_puts('u');
+	_puts('t');
+	_puts('s');
+	_puts('\n');
+}
