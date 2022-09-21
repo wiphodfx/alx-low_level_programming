e4c026d0b2c786929f59db96ab06e94e0685d08d
wiphodfx <wifidifx@gmail.com>
2022-09-21 13:37:14 -0700

    Task 6

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,3 +4,17 @@
  * string_toupper - changes all lowercase letters to a string to uppercase
  * @str: string
  * Return: char
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
