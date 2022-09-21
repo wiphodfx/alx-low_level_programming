2fec50d96b9351ae20883624d8a70c2abb6f627b
wiphodfx <wifidifx@gmail.com>
2022-09-21 13:32:29 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,13 +8,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int *start_c, *end_c;
+	int *start_c, *end_c, c;
 	int i;
 
 	start_c = a;
 	end_c = a;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n - 1; i++)
 	{
 		end_c++;
 	}
