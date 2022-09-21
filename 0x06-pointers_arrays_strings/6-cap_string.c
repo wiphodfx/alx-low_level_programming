f2ba9805df7f47d60497ffe5ae9b3f5417b7281a
wiphodfx <wifidifx@gmail.com>
2022-09-21 13:51:52 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,7 +8,7 @@
 char *cap_string(char *s)
 {
 	int count = 0, i;
-	int separators[] = {32, 9, 10, 44, 59, 46,33, 63, 34, 40, 41, 123, 125};
+	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (*(s + ount) >= 97 && *(s + count) <= 122)
 		*(s + count) = *(s + count) - 32;
@@ -19,7 +19,7 @@ char *cap_string(char *s)
 		{
 			if (*(s + count) == separators[i])
 			{
-				if((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
 					*(s + (count + 1)) = *(s + (count + 1)) - 32;
 				break;
 			}
