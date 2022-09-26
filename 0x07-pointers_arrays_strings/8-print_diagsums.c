a81bf55e03659214ef34d52aa6e32366ff6b232c
wiphodfx <wifidifx@gmail.com>
2022-09-26 13:43:54 -0700

    Task 7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include "main"
+
+/**
+ * print_diagsums - the sum of the two diagonals of a square
+ * @a: 2d arrays of chars
+ * @size: number of matrix
+ * Return void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum1, sum2;
+
+	sum1 = 0;
+	sum2 = 0;
+
+	i = 0;
+	while (i < size)
+	{
+		sum1 = sum1 + *(a + 1 * size + 1);
+		sum2 = sum2 + *(a + i * size + size - i - 1);
+		i++;
+	}
+	printf("%i, %i\n", sum1, sum2);
+}
