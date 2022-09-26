e7e94956edc2aecaae70ea75a3f990c8e000f5d3
wiphodfx <wifidifx@gmail.com>
2022-09-26 12:09:05 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes of the memory area
+ * pointd to by s
+ * @n: value of bytes in memory
+ * @b: value of constant byte
+ * @s: pointer to the memory
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int j;
+
+	/*using a for loop*/
+	for (j = 0; j < n; j++)
+	{
+		*(s + i) = b; /*incrementing s by 1*/
+	}
+	return (s);
+}
