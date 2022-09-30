802e9c1d16a5eb339682d58a9f51f0a3367857ba
wiphodfx <wifidifx@gmail.com>
2022-09-30 05:57:14 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: counts the number of input
+ * @argv: holds the value entered
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0;  i < argc; i++)
+	{
+		argc = argc + atoi(argv[i]);
+	}
+	printf("%d\n", argc);
+	return (0);
+}
+
