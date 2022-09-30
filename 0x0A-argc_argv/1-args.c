ee8580bb61d99707ef4dc45bc360c9cc315894f1
wiphodfx <wifidifx@gmail.com>
2022-09-30 06:38:55 -0700

    Task !

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,12 +10,5 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-
-		for (i = 0;  i < argc; i++)
-		{
-			argc = argc + atoi(argv[i]);
-		}
-		printf("%d\n", argc);
-		return (0);
+	printf("%d\n", argc);
 }
