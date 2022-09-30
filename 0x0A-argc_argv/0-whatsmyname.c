ff8266c428823d5ac04eeafd3ca841569a24d114
wiphodfx <wifidifx@gmail.com>
2022-09-30 04:58:37 -0700

    Task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its name
+ * followed by a newline
+ * @argc: counts the number of inputs
+ * @argv: value of the input
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("My name is: %s\n", argv[1]);
+	return (0);
+}
