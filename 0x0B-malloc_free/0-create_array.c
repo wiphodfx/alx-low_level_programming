33d628595ad5a8959cd7bae7a5a8f6414289b104
wiphodfx <wifidifx@gmail.com>
2022-10-03 07:43:39 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
+#include "main.h"
 #include <stdio.h>
 
-/** 
+/**
  * create_array - create an of chars
  * and initiailizes it with specific char
  * @size: stores the count
@@ -12,22 +13,27 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char buffer;
 
-	i = 0;
-	while (i < size)
+	char "buffer";
+
+	if (size == 0)
 	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
+		return (NULL);
 	}
-	printf("\n");
-	return NULL;
+	buffer = (char *)malloc(size * sizeof(c));
+
+	if (buffer == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		i = 0;
+		while (i < size)
+		{
+			*(buffer + position) = c;
+			i++;
+		}
+		return (buffer);
+	}
 }
