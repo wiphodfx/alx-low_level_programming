de01b3e009e7c8751eb4de8ac70e5339472cb5aa
wiphodfx <wifidifx@gmail.com>
2022-10-04 00:38:14 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -24,7 +24,7 @@ char *create_array(unsigned int size, char c)
 			return (NULL);
 		}
 		buffer = (char *)malloc(size * sizeof(c));
-		if (buffer == size)
+		if (buffer == 0)
 		{
 			return (NULL);
 		}
