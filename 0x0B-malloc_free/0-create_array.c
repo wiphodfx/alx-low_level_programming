4f304fa366d12e587e8cb7e11d74f52c4131d1e2
wiphodfx <wifidifx@gmail.com>
2022-10-04 01:00:55 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,1 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars and
+ * initializes it with a specific char
+ * @size: size of the memory to print
+ * @c: the pointer address
+ * Return: NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *buffer;
+
+	i = 0;
+	while (i < size)
+	{
+		if (size == 0)
+		{
+			return (NULL);
+		}
+		buffer = (char *)malloc(size * sizeof(c));
+		*(buffer + i) = c;
+		i++;
+	}
+	return (buffer);
 }
