a65599d1b261dab6ebfb61daedc3abf01d745a91
wiphodfx <wifidifx@gmail.com>
2022-10-06 05:08:49 -0700

    Task adv

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,7 +19,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	if (sptr == NULL)
+	if (ptr == NULL)
 	{
 		sptr = malloc(new_size);
 
