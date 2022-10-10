a6b1d53441f02ecb4747a4bb7db8dfc6eb2e8cf3
wiphodfx <wifidifx@gmail.com>
2022-10-10 13:11:56 -0700

    Task on struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog_h"
+#include <stdio.h>
+
+/**
+ * print_dog - print the dog struct
+ * @d: dog to print
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	return;
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Age: %f\n", d->age);
+	printf("owner: %s\n", d->owner ? d->owner : "(nil)");
+}
