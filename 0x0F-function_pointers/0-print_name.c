db613b6718de8c1028c285592223683e13c76c53
wiphodfx <wifidifx@gmail.com>
2022-10-12 15:33:05 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,9 +2,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
+ * print_name - a function that prints a name.
  * @name: name to print
- * @f: pointer to the printing function
+ * @f: point to the printing function
  */
 void print_name(char *name, void (*f)(char *))
 {
