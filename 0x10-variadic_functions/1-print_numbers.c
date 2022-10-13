f14ec3db88eb02ca126e6b1938d44b7687ac4abd
wiphodfx <wifidifx@gmail.com>
2022-10-13 10:22:16 -0700

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list nums;
 	unsigned int indexer;
 
-	va_start(nums, n)
+	va_start(nums, n);
 
 	for (indexer = 0; indexer < n; indexer++)
 	{
