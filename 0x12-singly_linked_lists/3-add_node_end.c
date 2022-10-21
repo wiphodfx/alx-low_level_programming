2cb145bc943dc94ab448942c83aaa77acff656be
wiphodfx <wifidifx@gmail.com>
2022-10-21 03:42:47 -0700

    Task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <string.h>
 
 /**
  * list_t *add_node_end - adds a new node at the end of a list_t list.
@@ -15,12 +16,14 @@ list_t *add_node_end(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
+
 	dup = strdup(str);
 	if (str == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
+
 	for (len = 0; str[len];)
 		len++;
 
