50a2487fb63d9f2ce7d56e574f279cab11194aa6
wiphodfx <wifidifx@gmail.com>
2022-10-21 03:54:12 -0700

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * here - prints a string before the main
+ * function is executed
+ */
+void here(void)
+{
+	printf("(A tortoise, having pretty good sense of a hare's nature,"
+		       "\nchallenges one to a race.)\n");
+}
