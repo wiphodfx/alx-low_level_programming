9dc2895354e35bb50e376a93c80b30af64a55db3
wiphodfx <wifidifx@gmail.com>
2022-11-06 13:05:09 -0800

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,11 +22,11 @@ int create_file(const char *filename, char *text_content)
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0000);
 	w = write(o, text_content, len);
-	
+
 	if (o == -1 || w == -1)
 		return (-1);
 
 	close(o);
 
 	return (1);
-}	
+}
