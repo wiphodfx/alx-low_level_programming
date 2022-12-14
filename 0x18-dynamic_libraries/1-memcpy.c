497b2287958861b03a67ff1f8f175513d560dca3
wiphodfx <wifidifx@gmail.com>
2022-12-13 21:10:05 -0800

    Create 1-memcpy.c

---
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copies n bytes  from memory
+ * @dest: points to the memory
+ * @src: memory area to dest
+ * @n: bytes in memory
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n) /*Using a while loop*/
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
